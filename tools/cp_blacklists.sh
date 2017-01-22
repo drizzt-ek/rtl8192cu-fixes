60a136f694483493b618394c73f77a93f0ea2762
Krzysztof.Nowak <krzysztof.nowak@schibsted.pl>
2017-01-22 11:25:00 +0100

    tools

---
+++ tools/cp_blacklists.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cp ../blacklist-native-rtl8192.conf /etc/modprobe.d/
+cp ../8192cu-disable-power-management.conf /etc/modprobe.d/
---
+++ tools/cp_wpa_test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+rm  /etc/wpa_supplicant/wpa_supplicant.conf
+cp ./wpa_supplicant.conf_test /etc/wpa_supplicant/wpa_supplicant.conf
